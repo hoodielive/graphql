36f1074c6211ac7b7b2bb886187c111a375629c5
hoodielive <hoodielive@gmail.com>
2019-02-15 18:05:47 -0500

    it can calc floats too

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -7,6 +7,7 @@ const typeDefs = `
   type Query {
     add(a: Float!, b: Float!): Float!
     greeting(name: String, position: String): String!
+    grades: [Int!]!
     me: User!
     post: Post!
   }
@@ -40,6 +41,9 @@ const resolvers = {
       else {
         return 'Hallo!'
       }
+    },
+    grades(parent, args, ctx, info) {
+      return [99, 80, 93]
     },
     me() {
       return {
